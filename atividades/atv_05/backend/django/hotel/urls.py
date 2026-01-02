f9d9208f3cc48c49174c008ce4f9d34cc63d8771
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-01-02 05:12:51 -0300

    Criação dos arquivos do frontend

--- atividades/atv_05/backend/django/hotel/urls.py
+++ atividades/atv_05/backend/django/hotel/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from sistema.views import *
+
+router = DefaultRouter()
+router.register(r'Funcionario', FuncionarioViewSet)
+router.register(r'Funcionario_Servico', FuncionarioServicoViewSet)
+router.register(r'Hospede', HospedeViewSet)
+router.register(r'Hospede_Reserva', HospedeReservaViewSet)
+router.register(r'Pagamento', PagamentoViewSet)
+router.register(r'Quarto', QuartoViewSet)
+router.register(r'Reserva', ReservaViewSet)
+router.register(r'Reserva_Pagamento', ReservaPagamentoViewSet)
+router.register(r'Reserva_Servico', ReservaServicoViewSet)
+router.register(r'Servico', ServicoViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls)
 ]
--- atividades/atv_05/backend/django/sistema/views.py
+++ atividades/atv_05/backend/django/sistema/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class FuncionarioViewSet(ModelViewSet):
+    queryset = Funcionario.objects.all()
+    serializer_class = FuncionarioSerializer
+
+class FuncionarioServicoViewSet(ModelViewSet):
+    queryset = FuncionarioServico.objects.all()
+    serializer_class = FuncionarioServicoSerializer
+    
+class HospedeViewSet(ModelViewSet):
+    queryset = Hospede.objects.all()
+    serializer_class = HospedeSerializer
+
+class HospedeReservaViewSet(ModelViewSet):
+    queryset = HospedeReserva.objects.all()
+    serializer_class = HospedeReservaSerializer
+
+class PagamentoViewSet(ModelViewSet):
+    queryset = Pagamento.objects.all()
+    serializer_class = PagamentoSerializer
+
+class QuartoViewSet(ModelViewSet):
+    queryset = Quarto.objects.all()
+    serializer_class = QuartoSerializer
+
+class ReservaViewSet(ModelViewSet):
+    queryset = Reserva.objects.all()
+    serializer_class = ReservaSerializer
+
+class ReservaPagamentoViewSet(ModelViewSet):
+    queryset = ReservaPagamento.objects.all()
+    serializer_class = ReservaPagamentoSerializer
+
+class ReservaServicoViewSet(ModelViewSet):
+    queryset = ReservaServico.objects.all()
+    serializer_class = ReservaServicoSerializer
+
+class ServicoViewSet(ModelViewSet):
+    queryset = Servico.objects.all()
+    serializer_class = ServicoSerializer
