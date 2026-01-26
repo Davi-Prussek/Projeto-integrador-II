e69994e64f61de6dafeb1ac9ec2b98d94edae565
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-01-26 02:30:17 -0300

    foiiiiiiii, obrigado senhor

---
+++ atividades/atv_04/backend/django/escola/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+class Curso(models.Model):
+    nome_curso = models.CharField(max_length=50)    
+    def __str__(self):
+        return self.nome_curso
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    numero_matricula = models.IntegerField(unique=True)
+    data_nascimento = models.DateField()
+    RG = models.CharField(max_length=25)
+    CPF = models.CharField(max_length=30)
+    nome_mae = models.CharField(max_length=100)
+    nome_pai = models.CharField(max_length=100)
+    data_matricula = models.DateField()
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.nome}'
+
+class Disciplina(models.Model):
+    nome = models.CharField(max_length=30)
+    carga_horaria = models.IntegerField()
+
+class Professor(models.Model):
+    nome = models.CharField(max_length=100)
+    data_nascimento = models.DateField()
+    data_admissao = models.DateField()
+    RG = models.CharField(max_length=25)
+    CPF = models.CharField(max_length=30)
+
+class Turma(models.Model):
+    nome = models.CharField(max_length=15)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    ano = models.IntegerField()
+
+class Matricula(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    data_matricula = models.DateField()
+
+class Frequencia(models.Model):
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    data = models.DateField()
+    presente = models.BooleanField()
+    class Meta: 
+        unique_together = (('aluno','turma','disciplina','data'),)
